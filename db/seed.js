6cf2de7a2ba5e8b670bdbd03c040aabbe1c2b33d
codingzilla <aj_chavez@sbcglobal.net>
2021-08-17 20:34:56 -0500

    users-added

--- db/seed.js
+++ db/seed.js
@@ -1,20 +1,18 @@
 // const { client } = require("./index");
 
-const {
-  client,
-  getAllUsers, // new
-} = require("./index");
+const { client, getAllUsers, createUser } = require("./index");
 
 async function testDB() {
   try {
-    client.connect();
+    console.log("Starting to test database...");
 
     const users = await getAllUsers();
-    console.log(users);
+    console.log("getAllUsers:", users);
+
+    console.log("Finished database tests!");
   } catch (error) {
-    console.error(error);
-  } finally {
-    client.end();
+    console.error("Error testing database!");
+    throw error;
   }
 }
 
@@ -42,22 +40,64 @@ testDB();
 // this function should call a query which drops all tables from our database
 async function dropTables() {
   try {
-    await client.query(`
+    console.log("Starting to drop tables...");
 
+    await client.query(`
+      DROP TABLE IF EXISTS users;
     `);
+
+    console.log("Finished dropping tables!");
   } catch (error) {
-    throw error; // we pass the error up to the function that calls dropTables
+    console.error("Error dropping tables!");
+    throw error;
   }
 }
 
 // this function should call a query which creates all tables for our database
 async function createTables() {
   try {
-    await client.query(`
+    console.log("Starting to build tables...");
 
+    await client.query(`
+      CREATE TABLE users (
+        id SERIAL PRIMARY KEY,
+        username varchar(255) UNIQUE NOT NULL,
+        password varchar(255) NOT NULL
+      );
     `);
+
+    console.log("Finished building tables!");
   } catch (error) {
-    throw error; // we pass the error up to the function that calls createTables
+    console.error("Error building tables!");
+    throw error;
+  }
+}
+
+async function createInitialUsers() {
+  try {
+    console.log("Starting to create users...");
+
+    const albert = await createUser({
+      username: "albert",
+      password: "bertie99",
+    });
+
+    const sandra = await createUser({
+      username: "sandra",
+      password: "2sandy4me",
+    });
+
+    const glamgal = await createUser({
+      username: "glamgal",
+      password: "soglam",
+    });
+
+    console.log(albert, sandra, glamgal);
+
+    console.log("Finished creating users!");
+  } catch (error) {
+    console.error("Error creating users!");
+    throw error;
   }
 }
 
@@ -67,11 +107,13 @@ async function rebuildDB() {
 
     await dropTables();
     await createTables();
+    await createInitialUsers();
   } catch (error) {
     console.error(error);
-  } finally {
-    client.end();
   }
 }
 
-rebuildDB();
+rebuildDB()
+  .then(testDB)
+  .catch(console.error)
+  .finally(() => client.end());
